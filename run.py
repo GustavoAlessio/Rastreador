2e122913c35446b4e7b690f33fe6be3798c5807d
Gustavo Alessio <gustavo.alessio@gmail.com>
2025-04-10 17:33:34 -0300

    mo

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-import os
 from flask import Flask
 from app.webhook import webhook_bp
 
 app = Flask(__name__)
+
+# Registrando o blueprint
 app.register_blueprint(webhook_bp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=os.getenv("DEBUG", "False") == "True")
+    app.run(host="0.0.0.0", port=5000)
